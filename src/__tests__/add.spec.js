b1300babc695dc68abbf03b39bc030de7e91bddf
volodymyr.kravchuk <volodymyr.kravchuk@eleks.com>
2018-04-18 10:54:03 +0300

    add test display basic example

--- src/__tests__/add.spec.js
+++ src/__tests__/add.spec.js
@@ -1,3 +1,10 @@
 import * as chai from 'chai';
 
-chai.expect(true).to.be.equal(false);
+describe('some tests', () => {
+  it('should pass', () => {
+    chai.expect(true).to.be.equal(true);
+  });
+  it('should fail', () => {
+    chai.expect(true).to.be.equal(false);
+  });
+});
